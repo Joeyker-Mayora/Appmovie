28e68c3e2ca0cf9600db4ecac267fd27bece4487
Joeyker <oswaldo_avilez@outlook.es>
2023-07-17 13:52:58 -0300

    aja

--- src/Components/AuthLoyout.jsx
+++ src/Components/AuthLoyout.jsx
@@ -3,7 +3,7 @@ const AuthLoyout = () => {
   return (
     <div id="ofi" className="  min-h-screen flex items-center justify-center  text-white">
          <Outlet/>
-        <video className="" muted autoPlay loop>
+        <video className="" muted loop>
             <source  src="/../video/mv.mp4" type="video/mp4" />
         </video>
         <div className="capa"></div>
